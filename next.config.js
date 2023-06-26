adc1a71176a8907ca2bc9314564845362f2dffa9
BestVersion7 <hunterkfan@gmail.com>
2023-06-26 11:08:59 -0400

    initial clean

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        domains: [
+            "res.cloudinary.com",
+            "lh3.googleusercontent.com",
+            "images.squarespace-cdn.com",
+            "avatars.githubusercontent.com",
+        ],
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
